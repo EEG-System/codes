c90ab8bdb9a57ace26461935ed88de9e59fbef96
Jose Carlos Dominguez <jcdc1carlos@gmail.com>
2020-11-13 14:59:39 -0700

    Update ADS1299.c

--- SPI Communication/ADS1299.c
+++ SPI Communication/ADS1299.c
@@ -1,24 +1,21 @@
+/*
+ * ADS1299.c
+ *
+ *  Created on: Nov 1, 2019
+ *      Author: Gerardo Barreras
+ */
+
 #include <stdbool.h>
 #include <stdint.h>
-#include <ti/devices/msp432e4/driverlib/driverlib.h>
+#include "ti/devices/msp432e4/driverlib/driverlib.h"
 #include "ti/usblib/msp432e4/usblib.h"
 #include "ti/usblib/msp432e4/usb-ids.h"
 #include "ti/usblib/msp432e4/device/usbdevice.h"
 #include "ti/usblib/msp432e4/device/usbdbulk.h"
+#include "uartstdio.h"
+#include "pinout.h"
+#include "usb_bulk_structs.h"
 #include "ADS1299.h"
-
-#define CS_LOW     GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_2, 0x00)
-#define CS_HIGH    GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_2, GPIO_PIN_2)
-
-/*
- * PL0 -> START
- * PL1 -> RESET
- * PL2 -> CS
- * PL3 -> DRDY
- * PA2 -> SCKL
- * PA4 -> DIN
- * PA5 -> DOUT
- */
 
 //ADS functions
 uint8_t transfer(uint8_t Byte1) //SPI transaction
@@ -32,21 +29,21 @@ uint8_t transfer(uint8_t Byte1) //SPI transaction
 void ReadREG(uint8_t Start) //Read value from a register
 {
     uint8_t opcode;
-    CS_LOW;
+    GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_2, 0x00);
     opcode = Start + RREG_S;
     transfer(opcode);
     SysCtlDelay(80); //Delay added to allow ADS to decode command
     transfer(0x00);
     SysCtlDelay(80);
     Registers[Start] = transfer(0x00);
-    CS_HIGH;
+    GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_2, GPIO_PIN_2);
     return;
 }
 void ReadREGS(uint8_t Start, uint8_t End)   //Read value from multiple registers
 {
     uint32_t i;
     uint8_t opcode;
-    CS_LOW;
+    GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_2, 0x00);
     opcode = Start + RREG_S;
     transfer(opcode);
     SysCtlDelay(80); //Delay added to allow ADS to decode command
@@ -57,14 +54,14 @@ void ReadREGS(uint8_t Start, uint8_t End)   //Read value from multiple registers
         Registers[Start + i] = transfer(0x00);
         SysCtlDelay(80);
     }
-    CS_HIGH;
+    GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_2, GPIO_PIN_2);
     return;
 }
 
 void WriteREG(uint8_t Start, uint8_t Data) //Write data to a register
 {
     uint32_t opcode = Start + WREG_S;
-    CS_LOW;
+    GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_2, 0x00);
     transfer(opcode);
     SysCtlDelay(80);
     transfer(0x00);
@@ -72,7 +69,7 @@ void WriteREG(uint8_t Start, uint8_t Data) //Write data to a register
     transfer(Data);
     SysCtlDelay(80);
     transfer(0x00);
-    CS_HIGH;
+    GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_2, GPIO_PIN_2);
     return;
 }
 
@@ -80,7 +77,7 @@ void WriteREGS(uint8_t Start, uint8_t End, uint8_t Data) //Write same data to mu
 {
     uint32_t opcode = Start + WREG_S;
     int i;
-    CS_LOW;
+    GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_2, 0x00);
     transfer(opcode);
     SysCtlDelay(80); //Delay added to allow ADS to decode command
     transfer(End);
@@ -91,169 +88,116 @@ void WriteREGS(uint8_t Start, uint8_t End, uint8_t Data) //Write same data to mu
         SysCtlDelay(80);
     }
     transfer(0x00);
-    CS_HIGH;
+    GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_2, GPIO_PIN_2);
     return;
 }
 
 void _RESET() //Command to Reset ADS (Can be used instead of pins)
 {
-    CS_LOW;
+    GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_2, 0x00);
     transfer(RESET);
     SysCtlDelay(353);
-    CS_HIGH;
+    GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_2, GPIO_PIN_2);
     return;
 }
 
 void _SDATAC()  //Command to allow modification of registers on ADS1299
 {
-    CS_LOW;
+    GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_2, 0x00);
     transfer(SDATAC);
     SysCtlDelay(120);
-    CS_HIGH;
+    GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_2, GPIO_PIN_2);
     return;
 }
 void _RDATAC() //Command to be able to exit register modification and start acquiring samples
 {
-    CS_LOW;
+    GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_2, 0x00);
     transfer(RDATAC);
     SysCtlDelay(120);
-    CS_HIGH;
+    GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_2, GPIO_PIN_2);
     return;
 }
 
 void _START() //Command to start sample acquisition. (Can be used instead of pins)
 {
-    CS_LOW;
+    GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_2, 0x00);
     transfer(START);
-    CS_HIGH;
+    GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_2, GPIO_PIN_2);
     return;
 }
 
 void _STOP() //Command to stop sample acquisition. (Can be used instead of pins)
 {
-    CS_LOW;
+    GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_2, 0x00);
     transfer(STOP);
-    CS_HIGH;
+    GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_2, GPIO_PIN_2);
     return;
 }
 
 void ADSTEST(uint32_t numsamples, uint32_t bytesSend) //Command that acquires samples from ADS
 {
-//    tUSBRingBufObject sTxRing;
-//    int i, j, k;
-//    int samplecounter = 0;
-//    USBBufferInfoGet(&g_sTxBuffer, &sTxRing);
-//    // How much space is there in the transmit buffer?
-//    uint32_t ui32Space = USBBufferSpaceAvailable(&g_sTxBuffer);
-//    uint32_t ui32WriteIndex = sTxRing.ui32WriteIndex;
-//    if (ui32Space == BULK_BUFFER_SIZE - 1)
-//    {
-//        TXComplete = false;
-//
-//        while (samplecounter < numsamples)
-//        {
-//            while (GPIOPinRead(GPIO_PORTL_BASE, GPIO_PIN_3)); //Wait for DRDY
-//
-//            for (k = 0; k < NumDaisy; k++)
-//            {
-//                GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_2, 0x00); //cs low
-//                //Status Bytes
-//                for (i = 0; i < 3; i++)//Status Bytes
-//                {
-//                    g_pui8USBTxBuffer[ui32WriteIndex] = transfer(0x00);
-//                    ui32WriteIndex++;
-//                    ui32WriteIndex =
-//                            (ui32WriteIndex == BULK_BUFFER_SIZE) ?
-//                                    0 : ui32WriteIndex;
-//                }
-//                for (i = 0; i < 8; i++)
-//                {
-//                    for (j = 0; j < 3; j++)
-//                    {
-//                        g_pui8USBTxBuffer[ui32WriteIndex] = transfer(0x00);
-//                        ui32WriteIndex++;
-//                        ui32WriteIndex =
-//                                (ui32WriteIndex == BULK_BUFFER_SIZE) ?
-//                                        0 : ui32WriteIndex;
-//                    }
-//                }
-//                GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_2, GPIO_PIN_2); //cs hgh
-//            }
-//            samplecounter++;
-//        }
-//        USBBufferDataWritten(&g_sTxBuffer, bytesSend);
-//    }
+    tUSBRingBufObject sTxRing;
+    int i, j, k;
+    int samplecounter = 0;
+    USBBufferInfoGet(&g_sTxBuffer, &sTxRing);
+    // How much space is there in the transmit buffer?
+    uint32_t ui32Space = USBBufferSpaceAvailable(&g_sTxBuffer);
+    uint32_t ui32WriteIndex = sTxRing.ui32WriteIndex;
+    if (ui32Space == BULK_BUFFER_SIZE - 1)
+    {
+        TXComplete = false;
+
+        while (samplecounter < numsamples)
+        {
+            while (GPIOPinRead(GPIO_PORTL_BASE, GPIO_PIN_3)); //Wait for DRDY
+
+            for (k = 0; k < NumDaisy; k++)
+            {
+                GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_2, 0x00);
+                //Status Bytes
+                for (i = 0; i < 3; i++)//Status Bytes
+                {
+                    g_pui8USBTxBuffer[ui32WriteIndex] = transfer(0x00);
+                    ui32WriteIndex++;
+                    ui32WriteIndex =
+                            (ui32WriteIndex == BULK_BUFFER_SIZE) ?
+                                    0 : ui32WriteIndex;
+                }
+                for (i = 0; i < 8; i++)
+                {
+                    for (j = 0; j < 3; j++)
+                    {
+                        g_pui8USBTxBuffer[ui32WriteIndex] = transfer(0x00);
+                        ui32WriteIndex++;
+                        ui32WriteIndex =
+                                (ui32WriteIndex == BULK_BUFFER_SIZE) ?
+                                        0 : ui32WriteIndex;
+                    }
+                }
+                GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_2, GPIO_PIN_2);
+            }
+            samplecounter++;
+        }
+        USBBufferDataWritten(&g_sTxBuffer, bytesSend);
+    }
     return;
 }
 
 void TEST() //Setup ADS to acquire internal test signals
 {
-    WriteREG(CONFIG2, 0xD0);       //Modify Config2 for test signals
-    WriteREGS(CH1SET, CH8SET, Registers[5] | 0x05); //Modify all channels for test signals
-    ReadREG(CONFIG2);              //Read back written values
-    ReadREGS(CH1SET, CH8SET);
+    WriteREG(0x02, 0xD0);       //Modify Config2 for test signals
+    WriteREGS(0x05, 0x0C, Registers[5] | 0x05); //Modify all channels for test signals
+    ReadREG(0x02);              //Read back written values
+    ReadREGS(0x05, 0x0C);
     return;
 }
 
 void NORM() //Setup ADS for normal operation
 {
-    WriteREG(CONFIG2, 0xC0);       //Modify Config2 for normal signals
-    WriteREGS(CH1SET, CH8SET, Registers[5] & 0xF8);
-    ReadREG(CONFIG2);              //Read back written values
-    ReadREGS(CH1SET, CH8SET);
+    WriteREG(0x02, 0xC0);       //Modify Config2 for normal signals
+    WriteREGS(0x05, 0x0C, Registers[5] & 0xF8);
+    ReadREG(0x02);              //Read back written values
+    ReadREGS(0x05, 0x0C);
 }
-
-void ADS1299_init(uint32_t ui32SysClock)
-{
-    uint32_t bytesSend = 0;
-
-    //ADS Stuff Setup
-    delay_ms(128); //128ms delay needed to ensure the voltages on ADS1299 have stabilized
-
-    //Port that is going to be used to toggle a few of the pins.
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOL);
-    while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOL))
-    {
-    }
-    GPIOPinTypeGPIOOutput(GPIO_PORTL_BASE, GPIO_PIN_0); //START pin
-    GPIOPinTypeGPIOOutput(GPIO_PORTL_BASE, GPIO_PIN_1); //RESET pin
-    GPIOPinTypeGPIOOutput(GPIO_PORTL_BASE, GPIO_PIN_2); //CS pin
-    GPIOPinTypeGPIOInput(GPIO_PORTL_BASE, GPIO_PIN_3);  //DRDY pin
-
-    GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_1, GPIO_PIN_1); //Take RESET high
-    GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_2, GPIO_PIN_2); //Take CS high
-
-    /* Enable clocks to GPIO Port A and configure pins as SSI */
-    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-    while (!(MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOA)))
-    {
-    }
-
-    MAP_GPIOPinConfigure(GPIO_PA2_SSI0CLK); // PA2 -> SCLK
-    MAP_GPIOPinConfigure(GPIO_PA3_SSI0FSS);
-    MAP_GPIOPinConfigure(GPIO_PA4_SSI0XDAT0); // PA4 -> DIN
-    MAP_GPIOPinConfigure(GPIO_PA5_SSI0XDAT1); // PA5 -> DOUT
-    MAP_GPIOPinTypeSSI(GPIO_PORTA_BASE, (GPIO_PIN_2 | GPIO_PIN_3 |
-    GPIO_PIN_4 | GPIO_PIN_5));
-
-    /* Enable the clock to SSI-0 module and configure the SSI Master */
-    MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_SSI0);
-    while (!(MAP_SysCtlPeripheralReady(SYSCTL_PERIPH_SSI0)))
-    {
-    }
-
-    MAP_SSIConfigSetExpClk(SSI0_BASE, ui32SysClock, SSI_FRF_MOTO_MODE_1, //15 MHz clock, clock phase 0 polarity 1
-    SSI_MODE_MASTER,
-                          (ui32SysClock / 8), 8);
-    MAP_SSIEnable(SSI0_BASE);
-
-    /* Flush the Receive FIFO */
-    while (MAP_SSIDataGetNonBlocking(SSI0_BASE, &bytesSend));
-    //SPI setup is complete
-
-    _RESET();
-    _SDATAC(); //Enter SDATAC and don't exit until told to by computer
-    ReadREGS(0x00, 0x17); //Read All Registers
-}
-
 //end of ADS functions
+
