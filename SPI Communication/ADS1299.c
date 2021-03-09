3a40ca9709c7a5adedaa2f5097a5cdd3925b6d53
Jose Carlos Dominguez <jcdc1carlos@gmail.com>
2021-03-08 19:27:24 -0700

    Update ADS1299.c

--- SPI Communication/ADS1299.c
+++ SPI Communication/ADS1299.c
@@ -20,8 +20,8 @@
  * PL2 -> CLKSEL
  * PL3 -> DRDY
  * PA2 -> SCKL
- * PA4 -> DIN
- * PA5 -> DOUT
+ * PA4 (TX from the MSP) -> DIN (of the ADS)
+ * PA5 (RX from the MSP) -> DOUT (of the ADS)
  *
  */
 
@@ -36,14 +36,13 @@ uint8_t transfer(uint8_t Byte1) //SPI transaction
 
 uint8_t ReadREG(uint8_t Start) //Read value from a register
 {
-    uint8_t opcode;
+    uint8_t opcode = Start + RREG_S;
     CS_LOW;
 
-    opcode = Start + RREG_S;
     transfer(opcode);
     //Delay added to allow ADS to decode command
     delay_us(2);
-    transfer(0x00);
+    transfer(1-1); // n of registers - 1
     delay_us(2);
     Registers[Start] = transfer(0x00);
 
@@ -53,21 +52,22 @@ uint8_t ReadREG(uint8_t Start) //Read value from a register
 void ReadREGS(uint8_t Start, uint8_t End)   //Read value from multiple registers
 {
     uint32_t i;
-    uint8_t opcode;
+    uint8_t opcode = Start + RREG_S;
+
     CS_LOW;
-    opcode = Start + RREG_S;
+
     transfer(opcode);
     //Delay added to allow ADS to decode command
     delay_us(2);
-    transfer(End);
+    transfer(End - Start); // n of registers - 1
     delay_us(2);
-    for (i = 0; i <= End; i++)
+    for (i = 0; i <= (End - Start); i++)
     {
         Registers[Start + i] = transfer(0x00);
         delay_us(2);
     }
     CS_HIGH;
-    return;
+
 }
 
 void WriteREG(uint8_t Start, uint8_t Data) //Write data to a register
@@ -76,13 +76,13 @@ void WriteREG(uint8_t Start, uint8_t Data) //Write data to a register
     CS_LOW;
     transfer(opcode);
     delay_us(2);
-    transfer(0x00);
+    transfer(1-1); // n of registers - 1
     delay_us(2);
     transfer(Data);
     delay_us(2);
-    transfer(0x00);
+
     CS_HIGH;
-    return;
+
 }
 
 void WriteREGS(uint8_t Start, uint8_t End, uint8_t Data) //Write same data to multiple registers.
@@ -93,16 +93,15 @@ void WriteREGS(uint8_t Start, uint8_t End, uint8_t Data) //Write same data to mu
     transfer(opcode);
     //Delay added to allow ADS to decode command
     delay_us(2);
-    transfer(End);
+    transfer(End - Start); // n of registers - 1
     delay_us(2);
     for (i = 0; i <= (End - Start); i++)
     {
         transfer(Data);
         delay_us(2);
     }
-    transfer(0x00);
+
     CS_HIGH;
-    return;
 }
 
 void _RESET() //Command to Reset ADS (Can be used instead of pins)
@@ -111,7 +110,6 @@ void _RESET() //Command to Reset ADS (Can be used instead of pins)
     transfer(RESET);
     delay_us(9);
     CS_HIGH;
-    return;
 }
 
 void _SDATAC()  //Command to allow modification of registers on ADS1299
@@ -120,7 +118,6 @@ void _SDATAC()  //Command to allow modification of registers on ADS1299
     transfer(SDATAC);
     delay_us(3);
     CS_HIGH;
-    return;
 }
 void _RDATAC() //Command to be able to exit register modification and start acquiring samples
 {
@@ -128,7 +125,6 @@ void _RDATAC() //Command to be able to exit register modification and start acqu
     transfer(RDATAC);
     delay_us(3);
     CS_HIGH;
-    return;
 }
 
 void _START() //Command to start sample acquisition. (Can be used instead of pins)
@@ -136,7 +132,6 @@ void _START() //Command to start sample acquisition. (Can be used instead of pin
     CS_LOW;
     transfer(START);
     CS_HIGH;
-    return;
 }
 
 void _STOP() //Command to stop sample acquisition. (Can be used instead of pins)
@@ -144,7 +139,6 @@ void _STOP() //Command to stop sample acquisition. (Can be used instead of pins)
     CS_LOW;
     transfer(STOP);
     CS_HIGH;
-    return;
 }
 
 void ADS1299_read_data(uint8_t NumDaisy)
@@ -152,22 +146,19 @@ void ADS1299_read_data(uint8_t NumDaisy)
     int i, j, k, stat;
     int index = 0;
 
-    delay_us(9);
-    _SDATAC();
+
+    _RDATAC();  // Put the Device Back in RDATAC Mode
 
     //Take Start high (Start Conversions)
     START_HIGH;
-    //Delay stop first few samples from being zero
-
-    _RDATAC();  // Put the Device Back in RDATAC Mode
 
     // Wait for DRDY
     while(DRDY);
 
-
     // For loop to go as many times as devices connected to Daisy Chain
     for(i=0;i<NumDaisy;i++)
     {
+
         CS_LOW;
         /*
          * Status Bytes
@@ -180,7 +171,7 @@ void ADS1299_read_data(uint8_t NumDaisy)
             stat = (stat << 8) | byte;
         }
 
-        //  Read 24 bits of channel data in 8 3 byte chunks
+        //  Read 24 bytes of channel data in 8 3 byte chunks
         for(j=0;j<8;j++)
         {
             for(k=0;k<3;k++)
@@ -193,49 +184,41 @@ void ADS1299_read_data(uint8_t NumDaisy)
                 index++;
             }
         }
-
         CS_HIGH;
-
-
+        index = 0;
+        delay_us(2);
     }
-    index = 0;
+
     //_SDATAC();
-    //START_LOW;
+
+
 
 }
 
 void TEST() //Setup ADS to acquire internal test signals
 {
-    WriteREG(CONFIG2, 0xD0);          //Modify Config2 for test signals
-    //WriteREG(CONFIG3, 0xE0);        // If Using Internal Reference, Send This Comman
-    WriteREGS(CH1SET, CH8SET, 0x05); //Modify all channels for test signals
-    ReadREG(CONFIG2);                  //Read back written values
-    ReadREGS(CH1SET, CH8SET);
-    return;
+    _SDATAC();
+    WriteREG(CONFIG2, 0xD6);          //Modify Config2 for test signals
+    WriteREGS(CH1SET, CH8SET, 0x65); //Modify all channels for test signals
 }
 
 void NORM() //Setup ADS for normal operation
 {
-    WriteREG(CONFIG2, 0xC0);       //Modify Config2 for normal signals
-    WriteREGS(CH1SET, CH8SET, 0x00);
-    ReadREG(CONFIG2);              //Read back written values
-    ReadREGS(CH1SET, CH8SET);
+    _SDATAC();
+    WriteREGS(CH1SET, CH8SET, 0x60);
 }
 
-void SHORT() //Setup ADS for normal operation
+void SHORTY() //Setup ADS for normal operation (CCS gives error if we want this function to be named "SHORT")
 {
-    WriteREG(CONFIG2, 0xC0);       //Modify Config2 for normal signals
-    WriteREGS(CH1SET, CH8SET, 0x01);
-    ReadREG(CONFIG2);              //Read back written values
-    ReadREGS(CH1SET, CH8SET);
+    _SDATAC();
+    WriteREGS(CH1SET, CH8SET, 0x61);
 }
 
-void ADS1299_init(uint32_t ui32SysClock)
+void SPI_ADS_init(uint32_t ui32SysClock)
 {
     uint32_t bytesSend = 0;
 
     //ADS Stuff Setup
-    //delay_ms(131); //128ms delay needed to ensure the voltages on ADS1299 have stabilized
 
     //Port that is going to be used to toggle a few of the pins.
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOL);
@@ -250,7 +233,9 @@ void ADS1299_init(uint32_t ui32SysClock)
     // Delay for Power-On Reset and Oscillator Start-Up
     GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_1, GPIO_PIN_1); //Take RESET high
     GPIOPinWrite(GPIO_PORTL_BASE, GPIO_PIN_2, GPIO_PIN_2); //Take CS high
-    delay_ms(131);//128ms delay needed to ensure the voltages on ADS1299 have stabilized
+
+    //131ms delay needed to ensure the voltages on ADS1299 have stabilized
+    delay_ms(131);
 
     /* Enable clocks to GPIO Port A and configure pins as SSI */
     MAP_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
@@ -272,15 +257,13 @@ void ADS1299_init(uint32_t ui32SysClock)
     }
 
     MAP_SSIConfigSetExpClk(SSI0_BASE, ui32SysClock, SSI_FRF_MOTO_MODE_1, //12 MHz clock, clock phase 0 polarity 1
-    SSI_MODE_MASTER,
-                          (ui32SysClock/10),8);
+    SSI_MODE_MASTER, (ui32SysClock/10), 8);
     MAP_SSIEnable(SSI0_BASE);
 
     /* Flush the Receive FIFO */
     while (MAP_SSIDataGetNonBlocking(SSI0_BASE, &bytesSend));
     //SPI setup is complete
 
-    //ReadREGS(0x00, 0x17); //Read All Registers
 }
 
 //end of ADS functions
